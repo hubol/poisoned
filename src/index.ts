d1222dff020680b8a6413e09adf4b2878c812c3d
Hubol <hubol.gordon@gmail.com>
2020-12-22 00:56:25 -0600

    Fix stupid pixi thing that seems to happen when scaling a canvas

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {loadTexturesAsync} from "./typedAssets/textures";
 async function initialize()
 {
     const howls = Object.values(require("./typedAssets/sounds")) as Howl[];
+    (PIXI.settings.MIPMAP_TEXTURES as any) = false;
     PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
     await Promise.all([loadFontsAsync(), loadTexturesAsync(), loadHowlsAsync(howls)]);
     require("./game/game.ts");
