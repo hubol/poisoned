42b90dad991a56c989100e484d421f608ca8d937
Hubol <hubol.gordon@gmail.com>
2020-12-22 22:35:33 -0600

    Add some effects to hickory hill

--- src/game/scenes/hickoryHill.ts
+++ src/game/scenes/hickoryHill.ts
@@ -3,10 +3,38 @@ import {styles} from "../styles";
 import {sleep} from "pissant";
 import {sceneStage} from "../game";
 import {HeldHands} from "../../typedAssets/textures";
-import {Sprite} from "pixi.js";
+import {Container, Graphics, Sprite} from "pixi.js";
+import {Now} from "../../utils/now";
 
 export async function hickoryHill()
 {
+    function f(x: number)
+    {
+        const dx = x - Now.s;
+        return 58 + .4 * x + Math.abs(Math.sin((1 + .1 * dx) - dx)) * 3;
+    }
+
+    const terrain = new Graphics()
+        .withStep(() => {
+            terrain
+                .clear()
+                .beginFill(0x2657A0)
+                .drawRect(0, 0, 128, 128)
+                .beginFill(0xF2EFA4);
+            terrain.moveTo(0, 128);
+
+            const samples = 64;
+            const dx = 128 / samples;
+            for (let i = 0; i <= samples; i++)
+            {
+                const x = i * dx;
+                terrain.lineTo(x, f(x))
+            }
+
+            terrain.lineTo(128, 128);
+            terrain.closePath();
+        });
+
     const fromCenter = 12;
     const hubol = character(styles.hubol).at(64 - fromCenter, 100);
     hubol.subimage = 1;
@@ -19,7 +47,10 @@ export async function hickoryHill()
     const heldHands = Sprite.from(HeldHands)
         .at(56, 88);
 
-    sceneStage.addChild(heldHands, cole, hubol);
+    const characters = new Container().at(-4, 6);
+
+    sceneStage.addChild(terrain, characters);
+    characters.addChild(heldHands, cole, hubol);
 
     await sleep(5000);
     await cole.say("Have you fallen for me?");
