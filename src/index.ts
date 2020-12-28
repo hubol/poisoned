95ee593200efefa537964f3cfe8978823f7c05f5
Hubol <hubol.gordon@gmail.com>
2020-12-27 20:27:27 -0600

    port devMute

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import {loadHowlsAsync} from "./utils/resources/loadHowls";
 import {Howl} from "howler";
 import * as PIXI from "pixi.js";
 import {loadTexturesAsync} from "./typedAssets/textures";
+import {environment} from "./utils/environment";
+import {devMute} from "./game/dev/devMute";
 
 async function initialize()
 {
@@ -10,6 +12,10 @@ async function initialize()
     (PIXI.settings.MIPMAP_TEXTURES as any) = false;
     PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
     await Promise.all([loadFontsAsync(), loadTexturesAsync(), loadHowlsAsync(howls)]);
+
+    if (!environment.isProduction)
+        devMute();
+
     require("./game/game.ts");
 }
 
