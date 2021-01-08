8810339c072875ea0297bcd6a883f1b734d3913b
Hubol <hubol.gordon@gmail.com>
2021-01-08 15:58:59 -0600

    Change title and add loading indicator

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,27 @@ import {devMute} from "./game/dev/devMute";
 
 async function initialize()
 {
+    const loadingElement = createLoadingElement();
+
     const howls = Object.values(require("./typedAssets/sounds")) as Howl[];
     (PIXI.settings.MIPMAP_TEXTURES as any) = false;
     PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
     await Promise.all([loadFontsAsync(), loadTexturesAsync(), loadHowlsAsync(howls)]);
 
+    loadingElement.remove();
+
     if (!environment.isProduction)
         devMute();
 
     require("./game/game.ts");
+}
+
+function createLoadingElement()
+{
+    const loadingElement = document.createElement('p');
+    document.body.appendChild(loadingElement);
+    loadingElement.id = "loading";
+    return loadingElement;
 }
 
 window.onload = initialize;
